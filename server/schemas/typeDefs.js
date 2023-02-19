b4dfe91cb2cbcd4eae954c43a291a46c6a1b4d4f
Zack <zachary.berkley@gmail.com>
2023-02-18 20:37:23 -0500

    added schema folder, typeDefs, and resolvers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,22 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    savedBooks [String]
+  }
+
+
+  type Query {
+    users: [User]
+  }
+
+  type Mutation {
+    
+  }
+`;
+
+module.exports = typeDefs;
